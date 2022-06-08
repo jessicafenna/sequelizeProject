133b3460406c0e9a2384c391ef0a04342a276675
Jessica Fenna <jessicafenna@gmail.com>
2022-06-08 15:36:23 +0100

    2nd function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const yargs = require ("yargs");
 const { sequelize }  = require ("./db/connection")
-const { addMovie } = require ("./movie/functions");
+const { 
+    addMovie,
+    listMovies,
+    updateMovie
+ } = require ("./movie/functions");
 
 // parameters are like empty variables - until you run the function the variable value is empty, when we run it  yargsObj (parameter) = yargs.argv (the argument we passed to the function)
 const app = async (yargsObj) =>{
+    // sync() (sequelize method) is checking the tables in the database, if we are trying to create one that doesn't exist, it will create it for us (i.e. first time you run one of the functions below, creates table) - until sync runs it doesn't exist so you can't insert info into it 
     await sequelize.sync();
     try{ 
         if (yargsObj.add){ 
@@ -12,9 +17,10 @@ const app = async (yargsObj) =>{
             await addMovie({title: yargsObj.title, actor: yargsObj.actor});
         } else if (yargsObj.list){ 
             // list all movies from db
-
+            await listMovies();
         } else if (yargsObj.update){ 
             // take filter and update k/v pairs from yargsObj - send to update function and return success/failure message
+            await updateMovie({actor: yargsObj.actor}, {title: yargsObj.title});
         } else if (yargsObj.delete){ 
             // take filter k/v pair from yargsObj and send to delete function, send success/failure message
         } else { 
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -3,8 +3,34 @@ const Movie = require("./table");
 exports.addMovie = async (movieObj) =>{ 
     try{
         const newMovie = await Movie.create(movieObj);
-        console.log(newMovie);
+        console.log(`Successfully added ${newMovie.dataValues.title} to database`);
     } catch (error){ 
         console.log(error)
     }
-}   
+};
+
+exports.listMovies = async () => { 
+    try { 
+        const movies = await Movie.findAll();
+        for (let i = 0; i<movies.length; i++){
+            console.log(movies[i].dataValues.title, movies[i].dataValues.actor)
+        }
+    } catch (error){ 
+        console.log(error);
+    }
+}
+
+exports.updateMovie = async (updateObj, filterObj) => { 
+    try { 
+        const response = await Movie.update(updateObj, {where: filterObj});
+        // the response is an array (only has one item in it)
+        // so if the response at index is more than 0 (i.e. there has been at least one change, then it will show a success message)
+        if(response[0]>0){ 
+            console.log ("Successfully updated")
+        } else { 
+            console.log ("Something went wrong with update")
+        };
+    } catch (error){ 
+        console.log(error);
+    }
+}
